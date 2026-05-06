e8133b6e74a2d9f43976ea658a3cb851088708e0
cr-marcstevens <github@marc-stevens.nl>
2026-05-06 15:52:34 +0200

    build.sh more verbose

--- build.sh
+++ build.sh
@@ -6,10 +6,15 @@ cp index.html _site/
 for d in 20* ; do
 	if [ ! -d $d ]; then continue; fi
 	cd $d
+	echo "============= $d ==================="
 	
+	echo "=== bundle install"
 	bundle install
+	echo "=== bundle exec"
 	bundle exec jekyll build
+	echo "=== copy"
 	cp -Rv _site ../_site/$d
 	
+	echo "============= $d done ==================="
 	cd ..
 done
